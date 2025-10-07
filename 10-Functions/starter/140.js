a76666e77fcfb5729c577dde0cb8edf669273f4b
Elichka <elichka@gmail.com>
2025-10-07 18:49:01 -0400

    modified:   10-Functions/starter/140.js

--- 10-Functions/starter/140.js
+++ 10-Functions/starter/140.js
@@ -3,3 +3,53 @@
 //////////////////////////////////////////
 // 140. The call and apply Methods
 /////////////////////////////////////////
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} book the seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+//book(23, 'Sarah Williams'); DOESN"T WORK
+// CALL METHOD
+book.call(eurowings, 23, 'Sarah Williams'); // setting  this keyword to a specific object.
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// APPLY METHOD - take array of arguments. is not that frequently used in modern java script.  However, you can just use  call with spread operator on array.
+
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// BIND METHOD - next lecture.
