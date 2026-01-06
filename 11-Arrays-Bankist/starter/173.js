324c1873b4b9f4d9f96e04a1d30576dea0b3cbaf
Elichka <elichka@gmail.com>
2026-01-06 16:06:10 -0500

    173. Array Grouping

--- 11-Arrays-Bankist/starter/173.js
+++ 11-Arrays-Bankist/starter/173.js
@@ -9,4 +9,62 @@
 // LECTURES
 // 173. Array Grouping
 
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+  type: 'standard',
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+  type: 'premium',
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [],
+  interestRate: 0.7,
+  pin: 3333,
+  type: 'basic',
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+  type: 'premium',
+};
+
+const accounts = [account1, account2, account3, account4];
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const groupedMovements = Object.groupBy(movements, movement =>
+  movement > 0 ? 'deposits' : 'withdrawal'
+);
+
+console.log(groupedMovements);
+
+const groupedbyActivity = Object.groupBy(accounts, account => {
+  const movementCount = account.movements.length;
+  if (movementCount >= 8) return 'very active';
+  if (movementCount >= 4) return 'active';
+  if (movementCount >= 1) return 'moderate';
+  return 'inactive';
+});
+
+console.log(groupedbyActivity);
+
+// const groupedAccounts = Object.groupBy(accounts, account => account.type);
+
+// using object destructring syntax with curly braces {}
+const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
+
+console.log(groupedAccounts);
